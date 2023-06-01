542f99030b783536f899511785494edef65d5c25
ravenssz <106459804+ravenssz@users.noreply.github.com>
2023-06-01 16:12:26 -0300

    Adicionando TextField de filtrar na tela de listagem de fornecedores

--- src/main/java/projeto/telas/ListarFornecedores/TelaListarFornecedores.java
+++ src/main/java/projeto/telas/ListarFornecedores/TelaListarFornecedores.java
@@ -1,44 +1,35 @@
 package projeto.telas.ListarFornecedores;
 
 import java.awt.Font;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
-import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
 import projeto.ImagemDeFundo;
-import projeto.OuvinteBotaoFundoBranco;
 import projeto.TelaPadrao;
-
 import projeto.modelos.Fornecedor;
-import projeto.modelos.FornecedorFisico;
-import projeto.modelos.PessoaFisica;
-import projeto.modelos.PessoaJuridica;
 import ulitilidades.persistencia.Persistencia;
 import ultilidades.fabricas.FabricaJButton;
 import ultilidades.fabricas.FabricaJLabel;
-import ultilidades.fabricas.FabricaJRadionButton;
+import ultilidades.fabricas.FabricaJText;
 import ultilidades.fabricas.FabricasColors;
 import ultilidades.reporsitorio.CentralDeInformacoes;
 
 public class TelaListarFornecedores extends TelaPadrao {
 
 	private ImagemDeFundo background;
-
-	private JRadioButton rdPessoaFisica;
-	private JRadioButton rdPessoaJuridica;
 	private JButton btnDetalhes;
 	private DefaultTableModel modelo;
 	private JScrollPane scrol;
 	private JTable tabelaFornecedores;
 	private Persistencia p = new Persistencia();
 	private CentralDeInformacoes central = p.recuperarCentral("central");
+	private JTextField txtFiltro;
 
 	public TelaListarFornecedores(String titulo) {
 		super(titulo);
@@ -136,27 +127,19 @@ public class TelaListarFornecedores extends TelaPadrao {
 		JLabel lblListaDeFornecedores = FabricaJLabel.criarJLabel("Lista De Fornecedores", 210, 10, 460, 40,
 				FabricasColors.corLabelBranca, 25);
 		JLabel lblInfor = FabricaJLabel.criarJLabel(
-				"Escolha uma das opcões acima para filtrar os forncedores pelo tipo", 110, 90, 480, 40,
+				"Digite no campos abaixo o que deseja Filtrar", 95, 50, 480, 40,
 				FabricasColors.corLabelBranca, 13);
-
-		rdPessoaFisica = FabricaJRadionButton.criarRadionButton("Pessoa Fisica", 140, 50, 130, 50,
-				FabricasColors.corLabelBranca, "Clique aqui para filtrar por pessoas fisicas", 15);
-
-		rdPessoaJuridica = FabricaJRadionButton.criarRadionButton("Pessoa Fisica", 400, 50, 130, 50,
-				FabricasColors.corLabelBranca, "Clique aqui para filtrar por pessoas Juridicas", 15);
+		txtFiltro = FabricaJText.criarJTextField(95, 90, 300, 25, FabricasColors.corTxtField,
+				FabricasColors.corLabelBranca,"Física", 16);
 
 		btnDetalhes = FabricaJButton.criarJButton("Ver Detalhes", 280, 390, 120, 30, FabricasColors.corLabelBranca,
 				FabricasColors.CorRoxo, "Clique aqui para confimar o seu cadastro", 16);
 
-//		rdPessoaFisica.addItemListener(ouvinteFiltro);
-//		rdPessoaFisica.addMouseListener(ouvinte);
-//		rdPessoaJuridica.addItemListener(ouvinteFiltro);
-//		rdPessoaJuridica.addMouseListener(ouvinte);
 		
 		background.add(lblInfor);
+		background.add(txtFiltro);
 		background.add(btnDetalhes);
-		background.add(rdPessoaFisica);
-		background.add(rdPessoaJuridica);
+	
 		background.add(lblListaDeFornecedores);
 	}
 
@@ -165,13 +148,8 @@ public class TelaListarFornecedores extends TelaPadrao {
 		add(background);
 
 	}
-
-	public JRadioButton getrdPessoaFisica() {
-		return rdPessoaFisica;
-	}
-
-	public JRadioButton getrdPessoaJuridica() {
-		return rdPessoaJuridica;
+	public JTextField getTxtFiltro() {
+		return txtFiltro;
 	}
 
 	public JButton getBtnDetalhes() {
